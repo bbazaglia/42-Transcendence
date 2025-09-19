4649ca95ade4c17732fcec54dd8a063da04f67de
Peters <114668521+phspeters@users.noreply.github.com>
2025-09-18 22:11:52 -0300

    refactor: implement TOTP plugin and update authentication routes for 2FA setup

---
+++ backend/src/plugins/totp.js
@@ -0,0 +1,66 @@
+import fp from 'fastify-plugin';
+import { TOTP, Secret } from 'otpauth';
+import qrcode from 'qrcode';
+
+async function totpPlugin(fastify, opts) {
+    const TOTP_ALG = opts.algorithm || TOTP.defaults.algorithm;
+    const TOTP_DIGITS = opts.digits || TOTP.defaults.digits;
+    const TOTP_ISSUER = opts.issuer || TOTP.defaults.issuer
+    const TOTP_ISSUER_IN_LABEL = opts.issuerInLabel || TOTP.defaults.issuerInLabel;
+    const TOTP_PERIOD = opts.period || TOTP.defaults.period;
+
+    // Generates a new TOTP instance with a new secret.
+    function setupNewTotp(userLabel) {
+        return TOTP.generate({
+            algorithm: TOTP_ALG,
+            digits: TOTP_DIGITS,
+            issuer: TOTP_ISSUER,
+            label: userLabel,
+            issuerInLabel: TOTP_ISSUER_IN_LABEL,
+            period: TOTP_PERIOD
+        });
+    }
+
+    async function generateQRCode(totpInstance) {
+        try {
+            const url = totpInstance.toString();
+            return await qrcode.toDataURL(url);
+        } catch (error) {
+            fastify.log.error(error, 'Error generating QR code:');
+            throw new Error(`QR code generation failed: ${error.message}`);
+        }
+    }
+
+    function verifyTotp(token, secret) {
+        if (!token || !secret) {
+            fastify.log.warn('verifyTotp called without a token or secret.');
+            return false;
+        }
+        try {
+            // Create a temporary instance for validation.
+            const totp = new TOTP({
+                issuer: TOTP_ISSUER,
+                algorithm: TOTP_ALG,
+                digits: TOTP_DIGITS,
+                period: TOTP_PERIOD,
+                secret: Secret.fromBase32(secret),
+            });
+
+            const delta = totp.validate({ token, window: TOTP_WINDOW });
+            return delta !== null;
+        } catch (error) {
+            fastify.log.error(error, 'TOTP verification failed due to an unexpected error.');
+            throw new Error('An internal error occurred during TOTP verification.');
+        }
+    }
+
+    const totp = {
+        setup: setupNewTotp,
+        generateQRCode: generateQRCode,
+        verify: verifyTotp,
+    };
+
+    fastify.decorate('totp', totp);
+}
+
+export default fp(totpPlugin);

04b2aea6b7fff758807cb72145f819a4e4f4d3a2
Peters <114668521+phspeters@users.noreply.github.com>
2025-09-18 22:10:55 -0300

    refactor: update prisma client initialization to use a single instance with omitted fields

--- backend/src/lib/prismaClients.js
+++ backend/src/lib/prismaClients.js
@@ -4,12 +4,15 @@ const privatePrisma = new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
 });
 
-const prisma = privatePrisma.$extends({
-    result: {
+// TODO: use single prisma client and use "omit: { key: false }" in the query when needed
+const prisma = new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
+    omit: {
         user: {
-            email: { compute: () => undefined },
-            passwordHash: { compute: () => undefined },
-            twoFaSecret: { compute: () => undefined },
+            email: true,
+            passwordHash: true,
+            twoFaSecret: true,
+            googleId: true,
         },
     },
 });
